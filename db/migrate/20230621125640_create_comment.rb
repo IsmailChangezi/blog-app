dbf5bb649d3817907c090aec90fc5a33d998d742
Ismail Ali <ismailchangezi801@gmail.com>
2023-06-21 18:05:34 +0500

    Setup Rails and Create Migrations

---
+++ db/migrate/20230621125640_create_comment.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateComment < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.text :text
+      t.references :user, foreign_key: true
+      t.references :author, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+  end
+end
